d5738f4bd42cf708ecb9f635ef5f0aa299d75225
Antonina <antonina.stefanowska@gmail.com>
2020-04-10 21:46:33 +0000

    cleanup

--- backend/management/commands/clear_tokens.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from backend.models import TwitterToken, FacebookToken
-
-class Command(BaseCommand):
-    help = "clearing tokens"
-    def handle(self, *args, **options):
-        TwitterToken.objects.all().delete()
-        FacebookToken.objects.all().delete()
